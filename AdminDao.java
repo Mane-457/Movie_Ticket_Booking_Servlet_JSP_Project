454fc5b0faeb912973ffcc96b79fc09afb3b7d82
Mane-457 <160385579+Mane-457@users.noreply.github.com>
2024-02-28 13:08:05 +0530

    Update AdminDao.java

--- AdminDao.java
+++ AdminDao.java
@@ -1,15 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package cn.techtutorial.ctb.admin.dao;
+package Movies;
 
-// Data Access Object = DAO (dao)
+
 import cn.techtutorial.ctb.admin.model.Admin;
 import cn.techtutorial.ctb.admin.services.AdminService;
 import java.sql.*;
 
-// Data Access Object class
+
 public class AdminDao implements AdminService {
 
     private final Connection con;
@@ -21,24 +17,23 @@ public class AdminDao implements AdminService {
         this.con = con;
     }
 
-    // Create a method to write our sql Query and Retrieve data from DB
+   
     @Override
     public Admin logAdmin(String email, String password) {
 
         Admin admin = null;
         try {
-            //create a sql query    
-            query = "select * from admin where email=? and password=?";   // Must be select ALL
-            // prepare the statement
+            
+            query = "select * from admin where email=? and password=?";   
             pst = this.con.prepareStatement(query);
             pst.setString(1, email);
             pst.setString(2, password);
-            // execute the statement
+          
             rs = pst.executeQuery();
 
-            //use ResultSet to get data, store data
+           
             if (rs.next()) {
-                // after getting data, we set test data to Admin Model class
+             
                 admin = new Admin();
                 admin.setId(rs.getInt("aid"));
                 admin.setName(rs.getString("name"));
