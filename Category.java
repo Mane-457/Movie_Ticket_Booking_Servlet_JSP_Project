126fbab674a09ea5b452ff9da43be64427218b64
Mane-457 <160385579+Mane-457@users.noreply.github.com>
2024-02-28 13:10:37 +0530

    Update Category.java

--- Category.java
+++ Category.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package cn.techtutorial.ctb.admin.model;
-
-/**
- *
- * @author nnbil
- */
+package Movies;
 public class Category {
     private int id;
     private String name;
